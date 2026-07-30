2e28e33ed20cf5e919f174d1651bc1075c88adf9
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-07-30 20:37:40 +0530

    feat: add horizontal scrolling to terminal log viewer

--- src/trafficsync/terminal/LogListBox.java
+++ src/trafficsync/terminal/LogListBox.java
@@ -5,8 +5,12 @@ import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.AbstractListBox;
 import com.googlecode.lanterna.gui2.TextGUIGraphics;
+import com.googlecode.lanterna.gui2.Interactable;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 
 public class LogListBox extends AbstractListBox<Event, LogListBox> {
+    private int horizontalScroll = 0;
 
     public LogListBox(TerminalSize preferredSize) {
         super(preferredSize);
@@ -37,6 +41,16 @@ public class LogListBox extends AbstractListBox<Event, LogListBox> {
                 String levelText = String.format("%-8s", "[" + item.getLevel() + "]");
                 String text = time + " " + levelText + " " + item.getMessage();
 
+                int scroll = listBox.getHorizontalScroll();
+                String textToDraw = text;
+                if (scroll > 0) {
+                    if (scroll < text.length()) {
+                        textToDraw = text.substring(scroll);
+                    } else {
+                        textToDraw = "";
+                    }
+                }
+
                 graphics.setBackgroundColor(TextColor.ANSI.DEFAULT);
                 graphics.setForegroundColor(color);
 
@@ -44,7 +58,7 @@ public class LogListBox extends AbstractListBox<Event, LogListBox> {
                     graphics.enableModifiers(SGR.REVERSE);
                 }
 
-                String padded = String.format("%-" + graphics.getSize().getColumns() + "s", text);
+                String padded = String.format("%-" + graphics.getSize().getColumns() + "s", textToDraw);
                 if (padded.length() > graphics.getSize().getColumns()) {
                     padded = padded.substring(0, graphics.getSize().getColumns());
                 }
@@ -55,5 +69,23 @@ public class LogListBox extends AbstractListBox<Event, LogListBox> {
                 }
             }
         });
+    }
+
+    public int getHorizontalScroll() {
+        return horizontalScroll;
+    }
+
+    @Override
+    public Interactable.Result handleKeyStroke(KeyStroke keyStroke) {
+        if (keyStroke.getKeyType() == KeyType.ArrowRight) {
+            horizontalScroll += 5;
+            invalidate();
+            return Interactable.Result.HANDLED;
+        } else if (keyStroke.getKeyType() == KeyType.ArrowLeft) {
+            horizontalScroll = Math.max(0, horizontalScroll - 5);
+            invalidate();
+            return Interactable.Result.HANDLED;
+        }
+        return super.handleKeyStroke(keyStroke);
     }
 }
